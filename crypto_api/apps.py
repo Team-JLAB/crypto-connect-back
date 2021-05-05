863c2e4bfe9a7baa6b47bfd1f5fdd6fd1ed5f16e
Logan Jones <loganemilyjones@mac.com>
2021-05-04 20:06:15 -0700

    rename project -> 'config' and app -> 'crypto_api'

--- crypto_api/apps.py
+++ crypto_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CryptoConfig(AppConfig):
+class CryptoApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crypto'
+    name = 'crypto_api'
