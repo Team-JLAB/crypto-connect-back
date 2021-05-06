df23ee4e64bd41cf9b3caadc164d8778887c918b
Logan Jones <loganemilyjones@mac.com>
2021-05-06 08:06:30 -0700

    remove unnecessary import

--- crypto_api/serializer.py
+++ crypto_api/serializer.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import User, Wallet
-from djmoney.money import Money
+
 
 class WalletSerializer(serializers.ModelSerializer):
     def validate_balance(self, balance):
         if balance < 0:
-            raise serializers.ValidationError('Wallet balance cannot be less than $0')
+            raise serializers.ValidationError(
+                'Wallet balance cannot be less than $0')
         return balance
-    
+
     class Meta:
         fields = ('balance', )
         model = Wallet
