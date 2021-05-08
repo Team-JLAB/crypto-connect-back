04dc09fb1f6d3fc3a070d69120a3c54f41bfe488
Logan Jones <loganemilyjones@mac.com>
2021-05-07 17:59:16 -0700

    gunicorn + whitenoise

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 import environ
 
 env = environ.Env(
@@ -62,7 +63,7 @@ REST_FRAMEWORK = {
     ),
 }
 
-REST_USE_JWT = False
+REST_USE_JWT = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -149,6 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
