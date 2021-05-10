4c87784127fb0faa5fc541e0de4e3459b3fffe44
Logan Jones <loganemilyjones@mac.com>
2021-05-10 15:10:59 -0700

    undo static changes

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,6 @@ import os
 import environ
 import dj_database_url
 from pathlib import Path
-from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,11 +24,11 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
-ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'), default='*')
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
 
 # Application definition
 
