66c7313ee6bdff846d5f40558acdda2607b4b68b
Logan Jones <loganemilyjones@mac.com>
2021-05-09 17:11:04 -0700

    config db from url instead

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import environ
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -102,6 +103,9 @@ DATABASES = {
         'PORT': env('DATABASE_PORT')
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
