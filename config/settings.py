cf9c154b5e46057c4a6930ac31561470ec806b64
Logan Jones <loganemilyjones@mac.com>
2021-05-11 12:51:06 -0700

    token auth instead

--- config/settings.py
+++ config/settings.py
@@ -161,12 +161,12 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
-REST_USE_JWT = True
+
 
 CORS_ALLOW_ALL_ORIGINS = env.bool("ALLOW_ALL_ORIGINS")
